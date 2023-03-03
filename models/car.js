9ac9abd33821545c09d2ff57b4ac5f7aeb9f2b72
minicycle <107970855+minicycle@users.noreply.github.com>
2023-03-03 12:26:03 +0700

    Add files via upload

---
+++ models/car.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const carSchema = new Schema({
+    name:{type:String, require:true, trim:true},
+    brand:{type:String, require:true, trim:true},
+    price: { type: Number},
+    category:{type:String, require:true, trim:true},
+    status:{type:String,default:'Stock'},
+    created: { type: Date, default: Date.now },
+
+  }, { 
+    toJSON: {virtuals: true},
+    collection: "cars",
+    versionKey: false
+});
+
+carSchema.virtual('price_vat').get(function(){
+    return (this.price*0.07) + this.price
+})
+
+const car = mongoose.model("Car", carSchema)
+
+module.exports = car
